bd1d0bfbad8b873cc9526108b880736af91b9ddf
Derlandy Belchior <d.belchior82@gmail.com>
2020-04-02 17:54:21 -0300

    Adicionando validaçao de rotas e testes

---
+++ backend/tests/unit/generateUniqueId.spec.js
@@ -0,0 +1,9 @@
+const generateUniqueId = require('./../../src/util/genereateUniqueId');
+
+describe('Generate Unique ID', () => {
+    it('should generate an unique ID', () => {
+        const id = generateUniqueId();
+
+        expect(id).toHaveLength(8);
+    })
+})
